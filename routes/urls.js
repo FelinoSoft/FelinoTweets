4cc5e7680189b75de1b3f8fc0c0c6c5f94dde2fc
Jaime <jai_mi_to@hotmail.es>
2016-05-03 16:59:32 +0200

    Primera aproximación de shortener

---
+++ routes/urls.js
@@ -0,0 +1,140 @@
+/**
+ * Este fichero es el encargado de ejecutar la operacion correspondiente a la
+ * peticion recibida, para la coleccion urls
+ */
+module.exports = function(app){
+
+    var url = require('../models/url.js');
+    var shortener = require('../src/shortener.js');
+    var database = require('../databse.js');
+
+    /* GET /url */
+    findAllUrls = function(req,res){
+        var response = {};
+        url.find(function(err,data){
+            if(err) {
+                response = {"error" : true, "message" : "Error fetching data"};
+            } else{
+                response = {"error" : false, "message" : data};
+            }
+            res.json(response);
+        });
+    };
+
+    /* GET /url/:short_url */
+    findById = function(req,res){
+        var response = {};
+        url.find({"short_url" : req.params.short_url}, function(err, data){
+            if(err || data.length == 0){
+                response = {"error" : true, "message" : "Error fetching data"};
+                res.json(response);
+            } else{
+                updateClicks(req, function(result){
+                  if(result.error){
+                    // Hubo error al updatear los clicks
+                    response = {"error" : true, "message" : "Error updating clicks"};
+                    res.json(response);
+                  } else{
+                    // No error
+                    res.redirect(data[0].long_url);
+                  }
+                });
+            }
+        });
+    };
+
+    /* POST /url */
+    addUrl = function(req, res){
+        var newUrl = new url();
+        var response = {};
+
+        // url info
+        database.getShortenID(function(result){
+          if(result === null){
+            // Error
+            response = {"error" : true, "message" : "Error adding data"};
+            res.json(response);
+          } else{
+            // No error
+            var num = result;
+            database.incrementShortenID(function(err){
+              if(err !== null){
+                // Error
+                response = {"error" : true, "message" : "Error adding data"};
+                res.json(response);
+              } else{
+                // No error
+                newUrl.short_url = shortener.encode(num);
+                newUrl.long_url = req.body.long_url;
+                newUrl.user_id = req.body.user_id;
+                newUrl.clicks = 0;
+
+                newUrl.save(function(err){
+                    if(err){
+                        response = {"error" : true, "message" : "Error adding data"};
+                        res.json(response);
+                    } else {
+                        findAllUrls(req,res);
+                    }
+                });
+              }
+            });
+          }
+        });
+    };
+
+    updateClicks = function(req,callback){
+        url.find({"short_url" : req.params.short_url},function(err,data){
+            data.clicks = data.clicks + 1;
+
+            data.save(function(err){
+                if(err){
+                    response = {"error" : true, "message" : "Error updating data"};
+                } else{
+                    response = {"error" : false, "message" : "Data is updated for " + req.params.short_url};
+                }
+                callback(response);
+            });
+        });
+    };
+
+    /* DELETE /url */
+    deleteAllUrls = function(req,res){
+        var response = {};
+
+        url.remove({}, function(err){
+            if(err){
+                response = {"error" : true, "message" : "Error deleting data"};
+                res.json(response);
+            } else{
+                findAllUrls(req,res);
+            }
+
+        });
+    };
+
+    /* DELETE /users/:id */
+    deleteUrl = function(req,res){
+        var response = {};
+        url.find({"short_url" : req.params.short_url}, function(err,data){
+            if(err){
+                response = {"error" : true, "message" : "Error fetching data"};
+            } else{
+                url.remove({"short_url" : req.params.short_url}, function(err){
+                    if(err){
+                        response = {"error" : true, "message" : "Error deleting data"};
+                        res.json(response);
+                    } else{
+                        findAllUrls(req,res);
+                    }
+                });
+            }
+        });
+    };
+
+    app.get('/url', findAllUrls);
+    app.get('/url/:id', findById);
+    app.post('/url', addUrl);
+    app.delete('/url', deleteAllUrls);
+    app.delete('/url/:id',deleteUrl);
+};
