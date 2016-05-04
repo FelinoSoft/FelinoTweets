1686fbbad620a1d418ee4d8272dc20d18daa3aa5
Jaime <jai_mi_to@hotmail.es>
2016-05-04 18:44:09 +0200

    Changed database adresses and deleted loles methods

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
-//var server = "mongodb://FelinoUser:superfelino1#@ds013310.mlab.com:13310/felinotweetsdb";
-var server = 'mongodb://127.0.0.1:27017/felinotweetsdb';
+var server = "mongodb://FelinoUser:superfelino1#@ds013310.mlab.com:13310/felinotweetsdb";
+//var server = 'mongodb://127.0.0.1:27017/felinotweetsdb';
 
 var user = require('./models/user');
 var twitter_account = require('./models/twitter_account');
@@ -10,51 +10,6 @@ var twitter_account = require('./models/twitter_account');
  * Database.js
  * Este fichero contiene todas las funciones de llamada a la base de datos
  */
-
- /**
-  * Funcion de coña
-  */
-
-function menudosLoles(callback){
-	MongoClient.connect(server,function(err,connection){
-			if (err) {
-				callback(err);
-			}
-
-      var collection = connection.collection('loles');
-
-			console.log('--Connected to database');
-
-      collection.insert({'Juajuaja':'hOSTIA k bueno'},
-        function(err, count){
-					connection.close();
-					if(!err) {
-						 callback("");
-					} else{
-						callback(err);
-					}
-			});
-		});
-}
-
-function buscarLoles(callback){
-	MongoClient.connect(server,function(err,connection){
-			if (err) {
-				callback(err);
-			}
-
-      var collection = connection.collection('loles');
-
-      collection.find().toArray(function(err, documents){
-					connection.close();
-					if(!err) {
-						 callback(documents);
-					} else{
-						callback(null);
-					}
-			});
-		});
-}
 
 function test_user(callback){
 
