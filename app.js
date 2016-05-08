b82793ba943cd48feff6fe5ce0ea1253cef1eb48
AMarquez94 <alejandromarquez94@gmail.com>
2016-05-08 13:42:34 +0200

    Cron working :D

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ var mongoose = require('mongoose');
 var passport = require('passport');
 var http = require('http');
 var databaseAddress = "mongodb://FelinoUser:superfelino1#@ds013310.mlab.com:13310/felinotweetsdb";
+//var databaseAddress = 'mongodb://127.0.0.1:27017/felino_tweets';
 var bodyParser = require('body-parser');
 var config = require('./config/config');
 var database = require('./src/database.js');
 var twitter = require('./src/twitter');
 var session = require('express-session');
 var job = require('./src/cron.js');
-//var databaseAddress = 'mongodb://127.0.0.1:27017/felinotweetsdb';
 
 require('./src/passport')(passport);
 
@@ -49,7 +49,8 @@ mongoose.connect(databaseAddress,function(err,res){
 
 // initialize twitter data
 twitter.initTwitter();
-job.initJob();
+//initialize cron for scheduled tweets
+job.initJob(twitter);
 // initialize server
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
--- src/cron.js
+++ src/cron.js
@@ -1,28 +1,33 @@
 var CronJob = require('cron').CronJob;
 
 var minutes = 1000 * 60;
-var twitter = require('./twitter');
+//var twitter = require('./twitter');
 var tweet = require('../models/scheduled_tweet.js');
 var account = require('../models/twitter_account.js');
 
-function initJob() {
-    console.log('starting');
+function initJob(twitter) {
+    console.log('Starting cron');
     var job = new CronJob({
         cronTime: '01 * * * * 0-6',
         onTick: function () {
             var currentDate = Math.round(Date.now() / minutes);
-            console.log('Date ' + currentDate);
             tweet.find(function (err, data) {
                 if (!err) {
                     data.forEach(function (e) {
-                        if (Math.round(e.date / minutes) == currentDate) {
-                            account.findById(e.account_id, function (err, res) {
+                        if (Math.round(e.date / minutes) == currentDate ||
+                            Math.round(e.date / minutes) < currentDate) {
+                            account.findOne({ 'account_id' : e.account_id}, function (err, res) {
                                 if (!err) {
-                                    twitter.postTweet(res.token, res.toke_secret, e.text, function (err, res) {
-                                        console.log('Tweet enviado');
+                                    twitter.postTweet(res.token, res.token_secret, e.text, function (err, result) {
+                                        if(!err) {
+                                            tweet.remove({'_id': e._id}, function (err, res) {
+                                            });
+                                        }
                                     });
                                 }
                             });
+                        } else{
+                            console.log('no es el momento');
                         }
                     });
                 }
