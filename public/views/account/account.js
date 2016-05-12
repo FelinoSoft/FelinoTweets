c9e76bd88a4f358768a864a5b93b66beb557b18e
Jaime <jai_mi_to@hotmail.es>
2016-05-12 13:17:53 +0200

    Update wip on account

--- public/views/account/account.js
+++ public/views/account/account.js
@@ -1,10 +1,16 @@
-var homeModule = angular.module('homeModule', [
+var accountModule = angular.module('accountModule', [
   'felinotweetsApp'
 ]);
 
-  homeModule.controller('homeController',
+  accountModule.controller('accountController',
     function($scope,$http,$location,auth){
-    console.log("HomeController inicializado");
+    console.log("AccountController inicializado");
+
+    $scope.implemented = false;
+
+
+    $scope.getAllDefaultPanels();
+    console.log(panels);
 
     $scope.logOut = function() {
       if(auth.logout){
@@ -12,10 +18,78 @@ var homeModule = angular.module('homeModule', [
       }
     };
 
-    $scope.getPanel = function(kind, index) {
-      if(kind == "TL"){
-        // Get data of timeline
+    $scope.getAllDefaultPanels = function(){
+      $scope.panels[0] = getPanel("Home");
+      $scope.panels[1] = getPanel("Mis tweets");
+      $scope.panels[2] = getPanel("Programados");
+      $scope.panels[3] = getPanel("Menciones");
+    };
 
+    $scope.getPanel = function(kind) {
+      if(kind == "Home"){
+        // Get data of timeline
+        if(!implemented){
+          // Mock data dpm
+          var tweet1 = {"author":"Lolazo","text":"Me pica el huevo izquierdo",
+            "imgLink":"https://pbs.twimg.com/profile_images/721419863609208832/3aBTZgMZ_400x400.jpg",
+            "nRetweets":"214", "nLikes":"12", "rted":false, "liked":false, "day":"7",
+            "month":"Feb", "year":"1994", "tweetLink":"http://www.google.es"
+          };
+          var tweet2 = {"author":"Lolazo","text":"Me pica el huevo derecho",
+            "imgLink":"https://pbs.twimg.com/profile_images/721419863609208832/3aBTZgMZ_400x400.jpg",
+            "nRetweets":"214", "nLikes":"12", "rted":true, "liked":true, "day":"7",
+            "month":"Feb", "year":"1994", "tweetLink":"http://www.google.es"
+          };
+          return {"kind":kind, "tweets":[tweet1,tweet2]};
+        }
+      } else if(kind == "Mis tweets"){
+        // Get data of own tweets
+        if(!implemented){
+          // Mock data dpm
+          var tweet3 = {"author":"Lolazo","text":"Mae mia si me pica",
+            "imgLink":"https://pbs.twimg.com/profile_images/721419863609208832/3aBTZgMZ_400x400.jpg",
+            "nRetweets":"214", "nLikes":"12", "rted":false, "liked":false, "day":"7",
+            "month":"Feb", "year":"1994", "tweetLink":"http://www.google.es"
+          };
+          var tweet4 = {"author":"Lolazo","text":"Me voy a rascar pero ya",
+            "imgLink":"https://pbs.twimg.com/profile_images/721419863609208832/3aBTZgMZ_400x400.jpg",
+            "nRetweets":"214", "nLikes":"12", "rted":true, "liked":true, "day":"7",
+            "month":"Feb", "year":"1994", "tweetLink":"http://www.google.es"
+          };
+          return {"kind":kind, "tweets":[tweet3,tweet4]};
+        } else if(kind == "Programados"){
+          // Get data of programmed tweets
+          if(!implemented){
+            // Mock data dpm
+            var tweet5 = {"author":"Lolazo","text":"Ojo que me he rascado!",
+              "imgLink":"https://pbs.twimg.com/profile_images/721419863609208832/3aBTZgMZ_400x400.jpg",
+              "nRetweets":"214", "nLikes":"12", "rted":false, "liked":false, "day":"7",
+              "month":"Feb", "year":"1994", "tweetLink":"http://www.google.es"
+            };
+            var tweet6 = {"author":"Lolazo","text":"Es coña",
+              "imgLink":"https://pbs.twimg.com/profile_images/721419863609208832/3aBTZgMZ_400x400.jpg",
+              "nRetweets":"214", "nLikes":"12", "rted":true, "liked":true, "day":"7",
+              "month":"Feb", "year":"1994", "tweetLink":"http://www.google.es"
+            };
+            return {"kind":kind, "tweets":[tweet5,tweet6]};
+          } else if(kind == "Menciones"){
+            // Get data of programmed tweets
+            if(!implemented){
+              // Mock data dpm
+              var tweet7 = {"author":"Lolazo","text":"@Lolazo Uff que bien se siente",
+                "imgLink":"https://pbs.twimg.com/profile_images/721419863609208832/3aBTZgMZ_400x400.jpg",
+                "nRetweets":"214", "nLikes":"12", "rted":false, "liked":false, "day":"7",
+                "month":"Feb", "year":"1994", "tweetLink":"http://www.google.es"
+              };
+              var tweet8 = {"author":"Lolazo","text":"@Lolazo Que me da algo lel",
+                "imgLink":"https://pbs.twimg.com/profile_images/721419863609208832/3aBTZgMZ_400x400.jpg",
+                "nRetweets":"214", "nLikes":"12", "rted":true, "liked":true, "day":"7",
+                "month":"Feb", "year":"1994", "tweetLink":"http://www.google.es"
+              };
+              return {"kind":kind, "tweets":[tweet7,tweet8]};
+            }
+          }
+        }
       }
     };
   });
